92eae7b4af3d7622566b9fe22409e4fad1d67ae4
kapoormayank <kapoormayank645@gmail.com>
2025-12-29 19:42:56 +0530

    AccessModifier.cpp

--- Object-Oriented_Programming/All-Inheritance.cpp
+++ Object-Oriented_Programming/All-Inheritance.cpp
@@ -1,53 +1,63 @@
+// C++ program to demonstrate all types of Inheritance
 #include<iostream>
 using namespace std;
+
+// Base Class
 class Animal{
     public:
     void eat(){
         cout<<"This animal eats food."<<endl;
     }
 };
-//single Inheritance
+
+// Derived class(Single Inheritance)
 class Dog : public Animal{
     public:
     void bark(){
         cout<<"The dog are bark."<<endl;
     }
 };
-//Multilevel Inheritance
+
+// Derived class(Multilevel Inheritance)
 class Puppy : public Dog{
     public:
     void flexibity(){
         cout<<"Puppy is very flexible!"<<endl;
     }
 };
+
+// Another Base class for Multiple Inheritance
 class Speed{
     public:
     void speed(){
         cout<<"Speed of animal!"<<endl;
     }
 };
-//Multiple Inheritance
+
+// Derived class(Multiple Inheritance)
 class fitness : public Animal,public Speed{
     public:
     void fast(){
         cout<<"Fast animal!"<<endl;
     }
 };
+
+// Main function
 int main(){
-    //Single Inheritance
+    // Single Inheritance
     cout<<"\n-- Single Inheritance --"<<endl;
-    Dog dog;
+    Dog dog; //Static Memory Allocation
     dog.eat();
     dog.bark();
-    //Mutilevel Inheritance
+    // Multilevel Inheritance
     cout<<"\n-- Multilevel Inheritance --"<<endl;
-    Puppy p1;
+    Puppy p1; //Static Memory Allocation
     p1.eat();
     p1.bark();
     p1.flexibity();
-    //Mutliple Inheritance
+    // Multiple Inheritance
     cout<<"\n-- Multiple Inheritance --"<<endl;
-    fitness f1;
+    fitness f1; //Static Memory Allocation
     f1.eat();
     f1.speed();
     f1.fast();
--- Object-Oriented_Programming/Polymorprism.cpp
+++ Object-Oriented_Programming/Polymorprism.cpp
@@ -1,6 +1,9 @@
+// C++ program to demonstrate Polymorphism using Virtual Functions
 #include<iostream>
 #include<string>
 using namespace std;
+
+// Base class
 class Vehicle{
     protected:
     string brand;
@@ -12,43 +15,45 @@ class Vehicle{
         this->model = y;
         this->year = z;
     }
-    void details(){
+    // Virtual function
+    virtual void details(){
         cout<<"Brand of vechicle : "<<this->brand<<endl;
         cout<<"Model of vechicle : "<<this->model<<endl;
         cout<<"Year of vechicle : "<<this->year<<endl<<endl;
     } 
 };
+
+// Derived class
 class Car : public Vehicle {
     float mileage;
     public:
     Car(string x,string y,int z,float mileage): Vehicle(x,y,z),mileage(mileage){
     }
-    void details(){
+    // Overriding the base class function
+    void details() override{
         cout<<"Car name : "<<model<<endl;
         cout<<"Brand of car : "<<brand<<endl;
         cout<<"Year of car : "<<year<<endl;
         cout<<"Mileage of car : "<<mileage<<endl<<endl;
     }
 };
-int main(){
-    
-    //Parent class object
 
+// Main function
+int main(){
+    //Parent class object
+    cout<<"\n-- Parent class object --"<<endl;
     Vehicle* vechicle = new Vehicle("Honda","Civic",2019);
     vechicle->details();
-    
     //Child class object
-
+    cout<<"\n-- Child class object --"<<endl;
     Car* car1 = new Car("Toyota","Corolla",2020,15.5f);
     car1->details();
-    
-    //Override function are perform and polyprism is achieved
-
+    //Parent class object refers to child class object
+    cout<<"\n-- Parent class object refrers to Child class object --"<<endl;
     Vehicle* car2 = new Car("Ford","Mustang",2021,10.5f);
     car2->details();
-    
-    //Never parent class cannot inherit from the child class
-
+    // Invalid: Child class object refres to parent class object
+    // cout<<"\n-- Child class object refers to Parent class object --"<<endl;
     // Car* car3 = new Vehicle("BMW","X5",2022);
     // car3->details();
     return 0;
