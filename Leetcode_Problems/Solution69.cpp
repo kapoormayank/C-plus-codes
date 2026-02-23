f3c30ab7389d6502f6539c5985a9657fbdd84c92
kapoormayank <kapoormayank645@gmail.com>
2026-02-23 15:53:49 +0530

    LeetCode Problems Solution

---
+++ Leetcode_Problems/Solution69.cpp
@@ -0,0 +1,19 @@
+// LeetCode Problem 69: Sqrt(x)
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Function to calculate the integer square root of a number
+int mySqrt(int x) {
+    return (int)sqrt(x);
+}
+
+// Main function
+int main() {
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    cout<<"Square root of "<<x<<" is "<<mySqrt(x)<<endl;
+    return 0;
+}
+
